4ecdad6a00738177eb313391f3063d9fc6a912a2
ASUS <ASUS@DESKTOP-L6MVLAR>
2023-08-30 11:15:44 -0500

    Prueba de escritorio para el pseudo codigo

---
+++ src/main/java/Ej5/Ej5.java
@@ -0,0 +1,18 @@
+
+package Ej5;
+public class Ej5 {
+
+    public static void main(String[] args) {
+        double SUMA, X, Y;
+        SUMA = 0;
+        X = 20;
+        SUMA = SUMA + X;
+        Y = 40;
+        X = X + Math.pow(Y,2);
+        SUMA = SUMA + (X/Y);
+        System.out.println("El valor de la suma es: "+SUMA);
+        
+        
+                
+    }
+}
